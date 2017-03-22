263cd22418e8b52b75958d3b2720ea1c45b6406f
Michael <mikewoodson4@gmail.com>
2017-03-21 20:13:07 -0700

    added some queries stuff

--- michael.sql
+++ michael.sql
@@ -19,4 +19,42 @@ drop view EloWithinDates;
 drop view LowestElo;
 
 6)
+create view AugustRatings as
+select E.country, E.name, E.elo, E.endDate
+from ClubEloEntry E
+where year(E.endDate) = [year] and month(E.endDate) = 'August';
 
+create view AugustTeams as
+select A.country, A.name, max(A.endDate) as lastRatingOfYear
+from AugustRatings A
+group by A.country, A.name;
+
+create view AugustElos as
+select *
+from AugustTeams T inner join AugustRatings R on T.country = R.country and
+T.name = R.name and T.lastRatingOfYear = R.endDate;
+
+create view MinElo as
+select min(A.elo) minElo
+from AugustElos A
+order by A.elo desc
+limit 32
+
+select A.country, A.name
+from AugustElos A
+where A.elo >= (select M.minElo from MinElo M);
+
+drop view AugustRatings;
+drop view AugustTeams;
+drop view AugustElos;
+drop view MinElo;
+
+7)
+create view TeamHistory as
+select C.country, C.name, C.elo
+from ClubEloEntry C
+where C.country = [country] and C.name = [name];
+
+select C.country, C.name
+from ClubEloEntry C
+where C.elo = (select min(T.elo) from TeamHistory T);
